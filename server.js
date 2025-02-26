d1b6cb225e20f22b759eea4a4bb58037d73931e4
TheDevFadi <fadi@geekpower.ca>
2025-02-25 23:06:54 -0500

    Started the bootcamps routes

--- server.js
+++ server.js
@@ -9,4 +9,21 @@ const app = express();
 // Set the port
 const PORT = process.env.PORT || 5000;
 
+// Routes
+app.get("/api/v1/bootcamps", (req, res) => {
+    res.status(200).json({success: true, msg: "Show all bootcamps!"});
+});
+app.get("/api/v1/bootcamps/:id", (req, res) => {
+    res.status(200).json({success: true, msg: `Display bootcamp ${req.params.id}`});
+});
+app.post("/api/v1/bootcamps", (req, res) => {
+    res.status(201).json({success: true, msg: "Create new bootcamp"});
+});
+app.put("/api/v1/bootcamps/:id", (req, res) => {
+    res.status(200).json({success: true, msg: `Update bootcamp ${req.params.id}`});
+});
+app.delete("/api/v1/bootcamps/:id", (req, res) => {
+    res.status(200).json({success: true, msg: `Delete bootcamp ${req.params.id}`});
+});
+
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
