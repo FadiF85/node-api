da855f1362da899826f0cffc5accb863698413ab
TheDevFadi <fadi@geekpower.ca>
2025-02-26 00:00:33 -0500

    Installed morgan development middleware

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
+// const logger = require("./middleware/logger");
+const morgan = require("morgan");
 
 // Load env vars
 dotenv.config( {path: "./config/config.env"});
@@ -11,6 +13,13 @@ const PORT = process.env.PORT || 5000;
 
 // Route files
 const bootcamps = require("./routes/bootcamps");
+
+// Use the middlewares
+// app.use(logger);
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
+
 
 // Mount routes
 app.use("/api/v1/bootcamps", bootcamps);
