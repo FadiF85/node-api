e09649e959b728b20724d07454dc1f69243cab73
TheDevFadi <fadi@geekpower.ca>
2025-03-02 21:25:18 -0500

    Updated the erro handler middleware to handle the validation rules of Mongoose

--- middleware/error.js
+++ middleware/error.js
@@ -6,13 +6,29 @@ const errorHandler = (err, req, res, next) => {
 
     let error = {...err};
 
-    console.log("Error name: ",err.name)
+    console.log("Fadi Error ", );
 
     // To check the error.name value and customize the error message based on that
     // Mongoose bad objectId
     if (err.name === "CastError") {
         const message = `Resource not found with id of ${err.value} in the database.`;
         error = new ErrorResponse(message, 404);
+    }
+
+    // Mongoose duplicate key
+    if (err.code === 11000) {
+        let duplicateValue = err.keyValue;
+        let keys = Object.keys(duplicateValue);
+        let duplicateKey = keys[0];
+
+        const message = `Duplicate value (${duplicateValue[duplicateKey]}) for the ${duplicateKey} property.`;
+        error = new ErrorResponse(message, 400);
+    }
+
+    // Models validation rules
+    if (err.name === "ValidationError") {
+        const message = Object.values(err.errors).map(item => item.message);
+        error = new ErrorResponse(message, 400);
     }
 
     res.status(error.statusCode || 500).json({
