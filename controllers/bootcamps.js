3f348b211e2b8349ff0c716bee2ae4f6dac82c8c
TheDevFadi <fadi@geekpower.ca>
2025-03-02 20:44:39 -0500

    Updated the error middleware to handle errors based on err.name value (CastError)

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,4 +1,5 @@
 const Bootcamp = require("../models/Bootcamp");
+const ErrorResponse = require("../utils/errorResponse");
 
 // @desc Get all bootcamps
 // @route GET /api/v1/bootcamps
@@ -8,7 +9,7 @@ exports.getBootcamps = async (req, res, next) => {
         const bootcamps = await Bootcamp.find();
         res.status(200).json({success: true, count: bootcamps.length, data: bootcamps});
     } catch (err) {
-        res.status(400).json({success: false});
+        next(err);
     }
 }
 
@@ -21,12 +22,11 @@ exports.getBootcamp = async (req, res, next) => {
 
         // In case there is no user with the passed ID
         if (!bootcamp) {
-            return res.status(400).json({success: false});
+            return next(new ErrorResponse(`Bootcamp not found with the id of ${req.params.id}`, 404));
         }
 
         res.status(200).json({success: true, data: bootcamp});
     } catch (err) {
-        // res.status(400).json({success: false});
         next(err);
     }
 }
@@ -39,7 +39,7 @@ exports.createBootcamp = async (req, res, next) => {
         const data = await Bootcamp.create(req.body);
         res.status(201).json({success: true, data: data});
     } catch (err) {
-        res.status(400).json({success: false});
+        next(err);
     }
 }
 
@@ -55,12 +55,12 @@ exports.updateBootcamp = async (req, res, next) => {
 
         // In case there is no user with the passed ID
         if (!bootcamp) {
-            return res.status(400).json({success: false});
+            return next(new ErrorResponse(`Bootcamp not found with the id of ${req.params.id}`, 404));
         }
 
         res.status(200).json({success: true, data: bootcamp});
     } catch (err) {
-        res.status(400).json({success: false});
+        next(err);
     }
 }
 
@@ -73,11 +73,11 @@ exports.deleteBootcamp = async (req, res, next) => {
 
         // In case there is no user with the passed ID
         if (!bootcamp) {
-            return res.status(400).json({success: false});
+            return next(new ErrorResponse(`Bootcamp not found with the id of ${req.params.id}`, 404));
         }
 
         res.status(200).json({success: true, data: bootcamp});
     } catch (err) {
-        res.status(400).json({success: false});
+        next(err);
     }
 }
--- middleware/error.js
+++ middleware/error.js
@@ -1,10 +1,23 @@
+const ErrorResponse = require("../utils/ErrorResponse");
+
 const errorHandler = (err, req, res, next) => {
     // Log to console for development
     console.log(err.stack);
 
-    res.status(500).json({
+    let error = {...err};
+
+    console.log("Error name: ",err.name)
+
+    // To check the error.name value and customize the error message based on that
+    // Mongoose bad objectId
+    if (err.name === "CastError") {
+        const message = `Resource not found with id of ${err.value} in the database.`;
+        error = new ErrorResponse(message, 404);
+    }
+
+    res.status(error.statusCode || 500).json({
         success: false,
-        error: err.message
+        error: error.message || "Server Error",
     });
 }
 
