0bc0deec56a4dcc5bb2e25a35cfb684eeaa1d2b0
TheDevFadi <fadi@geekpower.ca>
2025-03-08 15:00:25 -0500

    Implented the statics/aggregate on the Course model to calculate the avgCost field

--- models/Course.js
+++ models/Course.js
@@ -38,4 +38,42 @@ const CourseSchema = new mongoose.Schema({
     }
 });
 
+// Static method to get average of course
+CourseSchema.statics.getAverageCost = async function (bootcampId) {
+
+    console.log("Calculation average cost...")
+
+    const obj = await this.aggregate([
+        {
+            $match: { bootcamp: bootcampId },
+        },
+        {
+            $group: {
+                _id: "$bootcamp",
+                avgCost: { $avg: "$tuition"}
+            }
+        }
+    ]);
+
+    try {
+        // Get the related Bootcamp and update its avgCost
+        await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
+            avgCost: Math.ceil(obj[0].avgCost / 10) * 10
+        });
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+// Call getAverageCost after save
+CourseSchema.post("save", function () {
+    console.log("Post save is running.. ")
+    this.constructor.getAverageCost(this.bootcamp);
+});
+
+// Call getAverageCost before remove
+CourseSchema.post("deleteOne", { document: true, query: false }, async function () {
+    this.constructor.getAverageCost(this.bootcamp);
+});
+
 module.exports = mongoose.model("Course", CourseSchema);
