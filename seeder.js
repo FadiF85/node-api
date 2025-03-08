b0a2b324c418e8c30e27e2bc9078cf2aec5f37bd
TheDevFadi <fadi@geekpower.ca>
2025-03-07 22:35:28 -0500

    Seeded courses

--- seeder.js
+++ seeder.js
@@ -7,17 +7,20 @@ dotenv.config({path: "./config/config.env"});
 
 // Load models
 const Bootcamp = require("./models/Bootcamp");
+const Course = require("./models/Course");
 
 // Connect to DB
 mongoose.connect(process.env.MONGODB_URI);
 
 // Read JSON files
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf8"));
+const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`, "utf8"));
 
 // Import into DB
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps);
+        await Course.create(courses);
         console.log("Data imported..");
         process.exit();
     } catch (error) {
@@ -29,6 +32,7 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Bootcamp.deleteMany();
+        await Course.deleteMany();
         console.log("Data destroyed..");
         process.exit();
     } catch (error) {
